c24d84dd1ef8d11830aa3ca9b6c2d5bbe12bd5de
Ricky Egeland <ricky.egeland@gmail.com>
2023-06-09 13:42:54 -0500

    Update python versions

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="Identify SEP elevations above background in a time series (idsep) and analyze events individually (opsep)",
     entry_points={
