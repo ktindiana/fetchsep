348651ad104759e56a7a2fb0cf34a03c2c590ce6
Kathryn Whitman <kathryn.whitman@nasa.gov>
2023-09-30 20:24:53 -0500

    Added padding to SEP start and end times in prep obs workflow

--- fetchsep/idsep/make_event_list.py
+++ fetchsep/idsep/make_event_list.py
@@ -65,6 +65,53 @@ def read_sep_times_file(septimes_file):
     return sep_sttimes, sep_endtimes
     
 
+def revise_sep_times(startdate, enddate, sep_sttimes, sep_endtimes):
+    """ Adjust the start and end times of each SEP event (enhancement) to
+        include more time before and after the event.
+        
+        Will check to make sure that events did not happen in quick
+        succession.
+        
+        This subroutine is useful if SEPs will be run through OpSEP which
+        benefits from some lead time prior to the SEP enhancement.
+        
+        INPUT:
+        
+            :sep_sttimes: (1xn datetime array) n SEP start times
+            :sep_endtimes: (1xn datetime array) n SEP end times
+            
+        OUTPUT:
+        
+            :rev_sttimes: (1xn datetime array) n revised SEP start times
+            :rev_endtimes: (1xn datetime array) n revised SEP end times
+
+    """
+    rev_sttimes = sep_sttimes
+    rev_endtimes = sep_endtimes
+    
+    dt = datetime.timedelta(hours=6)
+    dt2 = dt + dt
+    
+    #First SEP start and last SEP end
+    diff = sep_sttimes[0] - startdate
+    if diff > dt:
+        rev_sttimes[0] = sep_sttimes[0] - dt
+    diff = enddate - sep_endtimes[-1]
+    if diff > dt:
+        rev_endtimes[-1] = sep_endtimes[-1] + dt
+
+    #Remaining SEP times
+    for i in range(len(sep_sttimes)-1,0,-1):
+        diff = sep_sttimes[i] - sep_endtimes[i-1]
+        if diff > dt:
+            rev_sttimes[i] = sep_sttimes[i] - dt
+    
+        if diff > dt2:
+            rev_endtimes[i-1] = sep_endtimes[i-1] + dt
+
+    return rev_sttimes, rev_endtimes
+
+
 def get_detector(date, detector_dates, detector):
     """ For a given date, find the right GOES detector
 
@@ -82,10 +129,9 @@ def get_detector(date, detector_dates, detector):
     return detector[idx]
 
 
-
-
-def make_event_list(septimes_file,detector_list,experiment,
-            flux_type,options,outfile):
+def make_event_list(str_startdate, str_enddate, septimes_file,
+            detector_list, experiment, flux_type, options, outfile,
+            revise=False):
     """ Read in SEP times and detector list, if applicable,
         and create a file in the appropriate format to run
         operational_sep_quantities.py in batch mode.
@@ -99,9 +145,22 @@ def make_event_list(septimes_file,detector_list,experiment,
         semi-colon. Flags includes TwoPeaks, SubtractBG, and
         DetectPreviousEvent.Options includes S14, Bruno2017,
         corrected, uncorrected.
-    
+        
+        Set revise = True to adjust the SEP start and end times
+        to include some padding at the front of the event. This is
+        useful if submitting times to OpSEP for processing.
+        
     """
-    
+    try:
+        startdate = datetime.datetime.strptime(str_startdate, "%Y-%m-%d %H:%M:%S")
+    except:
+        startdate = datetime.datetime.strptime(str_startdate, "%Y-%m-%d")
+    try:
+        enddate = datetime.datetime.strptime(str_enddate, "%Y-%m-%d %H:%M:%S")
+    except:
+        enddate = datetime.datetime.strptime(str_enddate, "%Y-%m-%d")
+
+
     header = "#Start Time,End Time,Experiment,Flux Type,Flags," \
             + "User Experiment Name,User Filename,Options,"\
             +"BGStart,BGEnd\n"
@@ -113,21 +172,35 @@ def make_event_list(septimes_file,detector_list,experiment,
         detector_dates, detector = read_detector_list(detector_list)
     
     sep_sttimes, sep_endtimes = read_sep_times_file(septimes_file)
-       
+    if revise:
+        sep_sttimes, sep_endtimes = revise_sep_times(startdate, enddate,
+                sep_sttimes, sep_endtimes)
+    
+    ####Start of full time range to first SEP time
+    line = ''
+    if detector_list != '':
+        det = get_detector(startdate, detector_dates, detector)
+        if det != None:
+            line = str_startdate + "," + str(sep_sttimes[0])\
+                + "," + det + "," + flux_type + ",,,," + options + ",,\n"
+    else:
+        line = str_startdate + "," + str(sep_sttimes[0]) + "," \
+            + experiment + "," + flux_type + ",,,," + options + ",,\n"
+    
+    out.write(line)
+    
+    ###SEP Times
     for i in range(len(sep_sttimes)):
         #Print out line for SEP event
         if detector_list != '':
             det = get_detector(sep_sttimes[i], detector_dates, detector)
             if det == None: continue
-        
-            line = str(sep_sttimes[i]) + "," + str(sep_endtimes[i])\
-                    + "," \
-                    + det + "," + flux_type + ",,,," + options + ",,\n"
-                    
+            line = str(sep_sttimes[i]) + "," + str(sep_endtimes[i]) + ","\
+                + det + "," + flux_type + ",,,," + options + ",,\n"
         else:
-                line = str(sep_sttimes[i]) + "," + str(sep_endtimes[i]) + "," \
-            + experiment + "," + flux_type + ",,,," + options + ",,\n"
-        
+            line = str(sep_sttimes[i]) + "," + str(sep_endtimes[i]) + "," \
+                + experiment + "," + flux_type + ",,,," + options + ",,\n"
+            
         out.write(line)
         
         #Print out line for time period between SEP events
@@ -137,18 +210,27 @@ def make_event_list(septimes_file,detector_list,experiment,
             det = get_detector(sep_endtimes[i], detector_dates,
                     detector)
             if det == None: continue
-        
             line = str(sep_endtimes[i]) + "," + str(sep_sttimes[i+1])\
-                    + "," + det + "," + flux_type + ",,,," \
-                    + options + ",,\n"
+                + "," + det + "," + flux_type + ",,,," + options + ",,\n"
         else:
-            line = str(sep_endtimes[i]) + "," + str(sep_sttimes[i+1])\
-                    + "," + experiment + "," + flux_type + ",,,," \
-                    + options + ",,\n"
+            line = str(sep_endtimes[i]) + "," + str(sep_sttimes[i+1]) + ","\
+                + experiment + "," + flux_type + ",,,," + options + ",,\n"
         
         out.write(line)
-        
-        
+
+
+    ####End of full time range from last SEP time
+    line = ''
+    if detector_list != '':
+        det = get_detector(enddate, detector_dates, detector)
+        if det != None:
+            line = str(sep_endtimes[-1]) + "," + str_enddate\
+                + "," + det + "," + flux_type + ",,,," + options + ",,\n"
+    else:
+        line = str(sep_endtimes[-1]) + "," + str_enddate + "," \
+            + experiment + "," + flux_type + ",,,," + options + ",,\n"
+    
+    out.write(line)
         
     out.close()
             
