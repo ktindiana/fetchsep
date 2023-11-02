2f0b02f9ead5aced96bafa062a8ffaaf4f178391
Ricky Egeland <ricky.egeland@gmail.com>
2023-11-02 11:31:48 -0500

    Fix bad bump2version call

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rickyegeland/fetchsep',
-    version='version='0.3.0'',
+    version='0.3.0',
     zip_safe=False,
 )
