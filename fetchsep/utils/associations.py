d57989d0267a8997110fab0965ba3bf60f249a66
Kathryn Whitman <kathryn.whitman@nasa.gov>
2025-11-04 13:57:46 -0600

    Add NOAA NCEI Xray science data
    Add subroutines to associations.py that download GOES Xray science data
    from the NOAA NCEI website onto the user's computer into the data directory.
    Read flare summary files (flsum) provided by NOAA and extract the information
    for a specific flare - identified by a timestamp that occurs during the flare.
    Return the info in a dictionary with keys named the same as the flare trigger
    block in CCMC's SEP Scoreboard JSON schema.
    
    Fix one column in the SRAG SEP list to read Xray, as in all the other columns,
    rather than X-ray.

--- fetchsep/utils/associations.py
+++ fetchsep/utils/associations.py
@@ -1,9 +1,23 @@
+from . import config as cfg
 import pandas as pd
 import os
 import sys
 import string
 import datetime
+import wget
+import urllib.request
 import numpy as np
+import netCDF4 as nc
+import cftime
+
+#GOES spacecraft categories needed for GOES X-ray data
+#Spacecraft in the GOES-R+ series
+goes_R = ["GOES-16", "GOES-17", "GOES-18", "GOES-19"]
+#Spacecraft prior to GOES-R
+goes_sc = ["GOES-08", "GOES-09","GOES-10","GOES-11",
+            "GOES-12","GOES-13","GOES-14","GOES-15"]
+#Spacecraft prior to GOES-08
+old_goes_sc = ["GOES-05", "GOES-06", "GOES-07"]
 
 
 def clean_file(input_path, output_path):
@@ -408,3 +422,358 @@ def srag_to_ccmc_flare(associations):
     del flare['urls']
     
     return flare
+
+
+##########################################################
+#################### X-RAY DATA ##########################
+##########################################################
+def goes_xray_sat_info():
+    """ Info to create filenames for NOAA's naming scheme for X-ray files.
+    
+    """
+
+    #Selecting the science-quality X-ray fluxes updated by NOAA in 2021+
+    #startdate and enddate are inclusive here
+    sat_info = {"GOES-08": {"url": 'goes08',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g08_',
+                        "flsum": 'sci_xrsf-l2-flsum_g08_',
+                        "version": '_v1-0-0',
+                        "startdate": datetime.datetime(1995,1,1),
+                        "enddate": datetime.datetime(2003,6,16)},
+              
+              "GOES-09": {"url": 'goes09',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g09_',
+                        "flsum": 'sci_xrsf-l2-flsum_g09_',
+                        "version": '_v1-0-0',
+                        "startdate": datetime.datetime(1996,4,1),
+                        "enddate": datetime.datetime(1998,7,28)},
+              
+              "GOES-10":  {"url": 'goes10',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g10_',
+                        "flsum": 'sci_xrsf-l2-flsum_g10_',
+                        "version": '_v1-0-0',
+                        "startdate": datetime.datetime(1998,7,1),
+                        "enddate": datetime.datetime(2009,12,1)},
+              
+              "GOES-11":  {"url": 'goes11',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g11_',
+                        "flsum": 'sci_xrsf-l2-flsum_g11_',
+                        "version": '_v1-0-0',
+                        "startdate": datetime.datetime(2006,6,1),
+                        "enddate": datetime.datetime(2008,2,10)},
+              
+              "GOES-12":  {"url": 'goes12',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g12_',
+                        "flsum": 'sci_xrsf-l2-flsum_g12_',
+                        "version": '_v1-0-0',
+                        "startdate": datetime.datetime(2003,1,10),
+                        "enddate": datetime.datetime(2007,4,12)},
+
+              "GOES-13":  {"url": 'goes13',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g13_',
+                        "flsum": 'sci_xrsf-l2-flsum_g13_',
+                        "version": '_v2-2-1',
+                        "startdate": datetime.datetime(2013,6,7),
+                        "enddate": datetime.datetime(2017,12,14)},
+               
+              "GOES-14":  {"url": 'goes14',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g14_',
+                        "flsum": 'sci_xrsf-l2-flsum_g14_',
+                        "version": '_v2-2-1',
+                        "startdate": datetime.datetime(2009,9,19),
+                        "enddate": datetime.datetime(2020,3,4)},
+               
+              "GOES-15":  {"url": 'goes15',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g15_',
+                        "flsum": 'sci_xrsf-l2-flsum_g15_',
+                        "version": '_v2-2-1',
+                        "startdate": datetime.datetime(2010,4,7),
+                        "enddate": datetime.datetime(2020,3,4)},
+
+               "GOES-16":   {"url": 'goes16',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g16_',
+                        "flsum": 'sci_xrsf-l2-flsum_g16_',
+                        "version": '_v2-2-0',
+                        "startdate": datetime.datetime(2017,2,7),
+                        "enddate": datetime.datetime(2025,4,6)},
+
+               "GOES-17":   {"url": 'goes17',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g17_',
+                        "flsum": 'sci_xrsf-l2-flsum_g17_',
+                        "version": '_v2-2-0',
+                        "startdate": datetime.datetime(2018,6,1),
+                        "enddate": datetime.datetime(2024,8,11)},
+
+               "GOES-18":   {"url": 'goes18',
+                        "avg1m": 'sci_xrsf-l2-avg1m_g18_',
+                        "flsum": 'sci_xrsf-l2-flsum_g18_',
+                        "version": '_v2-2-0',
+                        "startdate": datetime.datetime(2022,9,2),
+                        "enddate": datetime.datetime.now() - datetime.timedelta(hours=24)} #update when GOES-18 ends
+              }
+
+    return sat_info
+
+
+def check_goes_xray_science_data(startdate, enddate, experiment):
+    """ Check that GOES X-ray data is on your computer or download it from the NOAA
+        website. Return the filenames associated with the correct GOES data.
+        Reprocessed science X-ray data is available in daily files. 
+        
+        The following files are downloaded here:
+            - avg1m: 1-minute averages of XRS irradiances
+            - flsum: flare summary, flare detection flags such as start and peak
+        
+        The X-ray time profile can be plotted, analyzed from avg1m.
+        The flare start, peak, end times and values and integrated flux are
+        save in the flsum files.
+        
+        INPUTS:
+        
+        :startdate: (datetime) start of time period specified by user
+        :enddate: (datetime) end of time period entered by user
+        :experiment: (string) name of GOES satellite
+        
+        OUTPUTS:
+        
+        :filenames1: (string array) the files containing the GOES
+            XRS data that span the desired time range
+            (monthly files)
+        
+    """
+    avgfiles = [] #X-ray time series
+    flsumfiles = [] #flare summary with magnitude, start, peak, end, etc
+
+    if experiment in old_goes_sc:
+        print("{experiment} X-ray science data is not yet available from NOAA. Returning.")
+        return avgfiles, flsumfiles
+
+        
+    #GOES XRS science data is stored in daily data files
+    td = enddate - startdate
+    NFILES = td.days #number of data files to download
+    if td.seconds > 0: NFILES = NFILES + 1
+
+    sat_info = goes_xray_sat_info()
+
+    #GOES-08 to -15 files like:
+    #https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes08/xrsf-l2-avg1m_science/1995/01/
+    #https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes09/xrsf-l2-flsum_science/1996/04/
+
+    #GOES-R like
+    #https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-avg1m_science/2017/02/
+    #https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-flsum_science/2017/02/
+
+
+    #for every day that data is required, check if file is present or
+    #needs to be downloaded.
+    for i in range(NFILES):
+        date = startdate + datetime.timedelta(days=i)
+        year = date.year
+        month = date.month
+        day = date.day
+        date_suffix = 'd%i%02i%02i' % (year,month,day)
+
+        if date < sat_info[experiment]['startdate'] or date > sat_info[experiment]['enddate']:
+            print(f"{experiment} X-ray science data is not available for {date}. Skipping.")
+            continue
+
+        #Filenames like:
+        #sci_xrsf-l2-avg1m_g08_d19950103_v1-0-0.nc
+        #sci_xrsf-l2-flsum_g08_d19950103_v1-0-0.nc
+        avgfile = f"{sat_info[experiment]['avg1m']}{date_suffix}{sat_info[experiment]['version']}.nc"
+        flsumfile = f"{sat_info[experiment]['flsum']}{date_suffix}{sat_info[experiment]['version']}.nc"
+
+        #Check if files already on your computer
+        fullpath = os.path.join(cfg.datapath,'GOES',avgfile)
+        avg_exists = os.path.isfile(fullpath)
+        if avg_exists:
+            avgfiles.append(os.path.join('GOES',avgfile))
+        
+        fullpath = os.path.join(cfg.datapath,'GOES',flsumfile)
+        flsum_exists = os.path.isfile(fullpath)
+        if flsum_exists:
+            flsumfiles.append(os.path.join('GOES',flsumfile))
+
+        #If already have both files
+        if avg_exists and flsum_exists:
+            continue
+        
+
+        #If need to download
+        ###### GOES-08 to GOES-15 #####
+        if experiment in goes_sc:
+            avgurl = ('https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/%s/xrsf-l2-avg1m_science/%i/%02i/%s') % (sat_info[experiment]["url"],year,month,avgfile)
+             
+            flsumurl = ('https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/%s/xrsf-l2-flsum_science/%i/%02i/%s') % (sat_info[experiment]["url"],year,month,flsumfile)
+ 
+
+        ###### GOES-R ################
+        if experiment in goes_R:
+            avgurl = ('https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/%s/l2/data/xrsf-l2-avg1m_science/%i/%02i/%s') % (sat_info[experiment]["url"],year,month,avgfile)
+             
+            flsumurl = ('https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/%s/l2/data/xrsf-l2-flsum_science/%i/%02i/%s') % (sat_info[experiment]["url"],year,month,flsumfile)
+
+
+        #Download
+        if not avg_exists:
+            try:
+                try:
+                    urllib.request.urlopen(avgurl)
+                except:
+                    raise
+
+                wget.download(avgurl, os.path.join(cfg.datapath,'GOES',avgfile))
+                print(f"\ncheck_goes_science_xray_data: Downloaded {avgurl}")
+                avgfiles.append(os.path.join('GOES',avgfile))
+            except urllib.request.HTTPError:
+                print("Cannot access GOES file at " + avgurl + ". Skipping.")
+
+
+
+        if not flsum_exists:
+            try:
+                try:
+                    urllib.request.urlopen(flsumurl)
+                except:
+                    raise
+
+                wget.download(flsumurl, os.path.join(cfg.datapath,'GOES',flsumfile))
+                print(f"\ncheck_goes_science_xray_data: Downloaded {flsumurl}")
+                flsumfiles.append(os.path.join('GOES',flsumfile))
+            except urllib.request.HTTPError:
+                print("Cannot access GOES file at " + flsumurl + ". Skipping.")
+
+
+    return avgfiles, flsumfiles
+
+
+def get_unique(arr_in):
+    """ Given a list arr, return a list of unique values within arr. """
+    
+    try:
+        arr = arr_in.tolist()
+    except:
+        arr = arr_in
+        
+    unique = []
+    for val in arr:
+        if val in unique:
+            continue
+        else:
+            unique.append(val)
+            
+    return unique
+
+
+def flare_info(experiment, request_date):
+    """ Extract information for a specific flare on a specific datetime.
+        Enter a date within 15 minutes of flare start or end of for any time
+        between the flare start and end.
+        
+        INPUTS:
+        
+            :experiment: (string) GOES-08, etc
+            :date: (datetime) timestamp for a date related to a specific flare,
+                can be similar to flare start, peak, end. Will look compare date to
+                15 minutes from the flare start and end.
+                
+        OUTPUTS:
+        
+            Information available flsum files.
+            :info: (dictionary) carries flare start, peak, end, magnitude, fluence, etc.
+        
+    """
+    sat_info = goes_xray_sat_info()
+
+    flare_info = {} #Will contain info of associated flare, if found
+
+    year = request_date.year
+    month = request_date.month
+    day = request_date.day
+    date_suffix = 'd%i%02i%02i' % (year,month,day)
+
+    #Filenames like:
+    #sci_xrsf-l2-flsum_g08_d19950103_v1-0-0.nc
+    flsumfile = f"{sat_info[experiment]['flsum']}{date_suffix}{sat_info[experiment]['version']}.nc"
+
+    fullpath = os.path.join(cfg.datapath,'GOES',flsumfile)
+    flsum_exists = os.path.isfile(fullpath)
+    if not flsum_exists:
+        print(f"flare_info: Cannot find file {fullpath}. Run check_goes_xray_science_data to download.")
+        return {}
+
+    data = nc.Dataset(fullpath)
+
+    #long_name: Record start time. Neglects leap seconds since 1970-01-01.
+    #TIME VARIABLES STORED AS:
+    #comments: Time[UTC] =1 Jan 1970 00:00:00[UTC] + time[secs] + n[secs] where
+    #n = {0/number of leap secs since 1 Jan 1970} for a conversion function that {ignores/includes} leap secs.
+    #comment2: Duplicate timestamps occasionally occur when there are two adjacent flares and
+    #the second flare status=EVENT_START at the same time as an EVENT_END or POST_EVENT of the previous flare.
+    #units: seconds since 1970-01-01 00:00:00 UTC
+
+    flare_dates = cftime.num2pydate(data.variables["time"][:], data["time"].units)
+
+    #Get unique flare_ids
+    flare_ids = get_unique(data.variables["flare_id"][:])
+    td = datetime.timedelta(minutes=15) #request_date within 15 minutes of flare
+    select_idx = []
+    for flid in flare_ids:
+        #Compile all information per flare by extracting according to flare_id
+        idx = [i for i in range(len(data.variables["flare_id"][:])) if data.variables["flare_id"][i]==flid]
+        
+        #Check if request_date is associated with this flare
+        flst = None
+        flend = None
+        for ix in idx:
+            if data.variables["status"][ix] == "EVENT_START":
+                flst = flare_dates[ix]
+            if data.variables["status"][ix] == "EVENT_END":
+                flend = flare_dates[ix]
+
+        if flst != None and flend != None:
+            if (request_date >= flst-td) and (request_date <= flend+td):
+                select_idx = idx
+                print(f"Found flare {flst} to {flend}")
+
+        #Just in case there are two flares close together, give precedence
+        #to the one containing requested date
+        if flst != None and flend != None:
+            if (request_date >= flst) and (request_date <= flend):
+                select_idx = idx
+                print(f"Found flare {flst} to {flend}")
+
+    #If no flare found
+    if len(select_idx) == 0:
+        print(f"Flare not found for requested datetime {request_date}. Returning.")
+        return flare_info
+
+    #Compile flare info
+    flare_info = {  'catalog_id': np.nan, #flare_id
+                    'catalog': 'NOAA_NCEI_science_data',
+                    'start_time': None,
+                    'peak_time': None,
+                    'end_time': None,
+                    'intensity': np.nan,
+                    'class': None,
+                    'instrument': experiment,
+                    'integrated_intensity': np.nan, #start time to end time
+                }
+
+    #flare_id, status, xrsb_flux, flare_class, time, integrated_flux, flare_class
+    for ix in select_idx:
+        if data.variables["status"][ix] == "EVENT_START":
+            flare_info['start_time'] = flare_dates[ix]
+            flare_info['catalog_id'] = int(data.variables["flare_id"][ix])
+            
+        if data.variables["status"][ix] == "EVENT_PEAK":
+            flare_info['peak_time'] = flare_dates[ix]
+            flare_info['intensity'] = float(data.variables["xrsb_flux"][ix])
+            flare_info['class'] = data.variables["flare_class"][ix]
+            
+        if data.variables["status"][ix] == "EVENT_END":
+            flare_info['end_time'] = flare_dates[ix]
+            flare_info['integrated_intensity'] = float(data.variables["integrated_flux"][ix])
+
+    return flare_info
